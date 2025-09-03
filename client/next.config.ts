3e89a72806ba5d713f5cfbb9a9b13cc4db519c7b
Sakya Rasadi <89144593+sakyarasadi@users.noreply.github.com>
2025-09-03 12:56:08 +0530

    0.8.8v
    Fix netlify bugs

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,15 +1,8 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  trailingSlash: true,
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
